8aada805f30e62068091005406fd5f3017300ffb
avytran <tr.anhvy.work@gmail.com>
2026-02-07 15:49:07 +0700

    feat: adding index.ts file

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+import cors from "cors";
+
+import healthRoute from './routes/health.route';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+const {
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_NAME
+} = process.env;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/health', healthRoute);
+
+mongoose.connect(`mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@tetplannerpro.3yuf5p8.mongodb.net/${DB_NAME}?appName=TetPlannerPro`)
+    .then(async () => {
+        console.log("Connected to database!");
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    })
+    .catch(() => {
+        console.log("Connection failed!");
+    })
